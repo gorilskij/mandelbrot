ae2d3cea016fcdb25b848287a4c6e9845bc25907
gorilskij <43991400@users.noreply.github.com>
2025-01-11 01:56:34 +0100

    fix, calculate escape velocity

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,18 @@ use std::thread;
 
 const ITERATIONS: usize = 1000;
 
-fn calculate(c: Complex<f64>) -> bool {
+fn calculate(c: Complex<f64>) -> f64 {
     let mut z: Complex<f64> = Complex::ZERO;
-    for _ in 0..ITERATIONS {
+    for i in 0..ITERATIONS {
         z = z * z + c;
-        if z.is_infinite() || z.is_nan() { return false; }
+        if z.is_infinite() || z.is_nan() { return i as f64 / ITERATIONS as f64; }
     }
-    !z.is_infinite() && !z.is_nan()
+    if z.is_infinite() || z.is_nan() { return 1.0; }
+    0.0
 }
 
 fn main() {
-    let (w, h) = (2000, 2000);
+    let (w, h) = (32_768, 32_768);
 
     let (x_min, x_max) = (-1.5, 0.5);
     let (y_min, y_max) = (-1.5, 1.5);
@@ -26,9 +27,9 @@ fn main() {
     let x_range = x_max - x_min;
     let y_range = y_max - y_min;
 
-    let (tx, rx) = channel::<(u32, Vec<bool>)>();
+    let (tx, rx) = channel::<(u32, Vec<f64>)>();
 
-    let pbar = ProgressBar::new(h as u64);
+    let pbar = ProgressBar::new((h / 2 + 1) as u64);
 
     let buffer: ImageBuffer<Rgb<u8>, _> = ImageBuffer::new(w, h);
     let buffer = Arc::new(Mutex::new(buffer));
@@ -37,8 +38,8 @@ fn main() {
         let mut buffer = buffer_clone.lock().unwrap();
         for _ in 0..h / 2 + 1 {
             let (r, row) = rx.recv().unwrap();
-            for (c, ans) in row.into_iter().enumerate() {
-                let pixel = if ans { Rgb([255, 0, 0]) } else { Rgb([0, 0, 0]) };
+            for (c, val) in row.into_iter().enumerate() {
+                let pixel = Rgb([(val * 255.0) as u8, 0, 0]);
                 buffer.put_pixel(r, c as u32, pixel);
                 buffer.put_pixel(h - r - 1, c as u32, pixel);
             }
@@ -48,10 +49,10 @@ fn main() {
     (0..h / 2 + 1).into_par_iter().for_each(|r| {
         let mut row = Vec::with_capacity(w as usize);
         for c in 0..w {
-            let x = r as f64 / w as f64 * x_range + x_min;
-            let y = c as f64 / h as f64 * y_range + y_min;
-            let ans = calculate(Complex::new(x, y));
-            row.push(ans);
+            let x = c as f64 / w as f64 * x_range + x_min;
+            let y = r as f64 / h as f64 * y_range + y_min;
+            let val = calculate(Complex::new(x, y));
+            row.push(val);
         }
         tx.send((r, row)).unwrap();
         pbar.inc(1);
@@ -62,5 +63,5 @@ fn main() {
     pbar.finish();
     println!("ELAPSED {:?}", pbar.elapsed());
 
-    buffer.lock().unwrap().save("images/test.png").unwrap();
+    buffer.lock().unwrap().save("../images/aura_32k.png").unwrap();
 }
