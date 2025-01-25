d752ad1e7f0c7a2a16f2807de39adb24dd8fdd8a
gorilskij <43991400@users.noreply.github.com>
2025-01-25 09:11:26 +0100

    zoom works but rendering is messed up

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use euclid::{Point2D, UnknownUnit};
 use hsl::HSL;
 use indicatif::ProgressBar;
 use minifb::{Key, KeyRepeat, MouseMode, Window, WindowOptions};
@@ -7,6 +8,8 @@ use palette::{Mix, Srgb, rgb};
 use rayon::prelude::*;
 
 const ITERATIONS: usize = 2000;
+
+type Point = Point2D<f64, UnknownUnit>;
 
 fn calculate(c: Complex<f64>) -> f64 {
     let mut z: Complex<f64> = Complex::ZERO;
@@ -83,32 +86,50 @@ fn interpolate(
 fn sample_zoomed(
     src: &[u32],
     dest: &mut [u32],
+    // src and dest must match
     width: usize,
     height: usize,
-    /* [0,1] relative to the source */
-    center_x: f64,
-    center_y: f64,
-    multiplier: f64,
+    //
+    src_origin: Point,
+    src_view: f64,
+    //
+    dest_origin: Point,
+    dest_view: f64,
 ) {
     let fwidth = width as f64;
     let fheight = height as f64;
 
+    // src_origin and dest_origin are both absolute
+    // calculate dest_origin in the [0, 1] reference frame given by src
+    let dest_origin_rel_src = ((dest_origin - src_origin) / src_view).to_point();
+
     for dest_row in 0..height {
         for dest_col in 0..width {
-            let dest_y = dest_row as f64 / fheight;
-            let dest_x = dest_col as f64 / fwidth;
+            // [0, 1] coordinates relative to the reference frame given by dest
+            let point_rel_dest = Point::new(dest_col as f64 / fwidth, dest_row as f64 / fheight);
 
-            let src_y =
-                ((dest_y - center_y) / multiplier + center_y).clamp(0.0, (fheight - 1.0) / fheight);
-            let src_x =
-                ((dest_x - center_x) / multiplier + center_x).clamp(0.0, (fwidth - 1.0) / fwidth);
+            // calculate [0, 1] coordinates in the reference frame given by src
+            let point_rel_src =
+                (dest_origin_rel_src + point_rel_dest.to_vector() * (dest_view / src_view)).clamp(
+                    Point::zero(),
+                    Point::new((fwidth - 1.0) / fwidth, (fheight - 1.0) / fheight),
+                );
 
-            let tl = ((src_y * fheight) as usize, (src_x * fwidth) as usize);
-            let tr = ((src_y * fheight) as usize, (src_x * fwidth).ceil() as usize);
-            let bl = ((src_y * fheight).ceil() as usize, (src_x * fwidth) as usize);
+            let tl = (
+                (point_rel_src.y * fheight) as usize,
+                (point_rel_src.x * fwidth) as usize,
+            );
+            let tr = (
+                (point_rel_src.y * fheight) as usize,
+                (point_rel_src.x * fwidth).ceil() as usize,
+            );
+            let bl = (
+                (point_rel_src.y * fheight).ceil() as usize,
+                (point_rel_src.x * fwidth) as usize,
+            );
             let br = (
-                (src_y * fheight).ceil() as usize,
-                (src_x * fwidth).ceil() as usize,
+                (point_rel_src.y * fheight).ceil() as usize,
+                (point_rel_src.x * fwidth).ceil() as usize,
             );
 
             let color = interpolate(
@@ -116,7 +137,10 @@ fn sample_zoomed(
                 src[tr.0 * width + tr.1],
                 src[bl.0 * width + bl.1],
                 src[br.0 * width + br.1],
-                (src_y - src_y.floor(), src_x - src_x.floor()),
+                (
+                    point_rel_src.y - point_rel_src.y.floor(),
+                    point_rel_src.x - point_rel_src.x.floor(),
+                ),
             );
 
             dest[dest_row * width + dest_col] = color;
@@ -130,34 +154,24 @@ struct BufView(*mut u32);
 unsafe impl Send for BufView {}
 unsafe impl Sync for BufView {}
 
-fn render(
-    buf: &mut [u32],
-    w: usize,
-    h: usize,
-    // the coordinate of the top-left corner (x, y)
-    origin: (f64, f64),
-    view: f64,
-) {
-    let (x_min, y_min) = origin;
-    let x_range = w as f64 * view;
-    let y_range = h as f64 * view;
-
+fn render(buf: &mut [u32], width: usize, height: usize, origin: Point, view: f64) {
     let buf_view = BufView(buf.as_mut_ptr());
 
-    let pbar = &ProgressBar::new(h as u64);
-    (0..h).into_par_iter().for_each(move |r| {
+    let pbar = &ProgressBar::new(height as u64);
+    (0..height).into_par_iter().for_each(move |r| {
         let buf_view = buf_view;
 
-        for c in 0..w {
-            let x = c as f64 / w as f64 * x_range + x_min;
-            let y = r as f64 / h as f64 * y_range + y_min;
-            let val = calculate(Complex::new(x, y));
+        for c in 0..width {
+            let val = calculate(Complex::new(
+                c as f64 * view + origin.x,
+                r as f64 * view + origin.y,
+            ));
 
             // SAFETY: all writes are disjoint
             unsafe {
                 buf_view
                     .0
-                    .offset((r * w + c) as isize)
+                    .offset((r * width + c) as isize)
                     .write(render_pixel(val));
             }
         }
@@ -169,85 +183,117 @@ fn render(
 struct Buffer {
     base: Box<[u32]>,
     zoomed: Box<[u32]>,
-    multiplier: f64,
+    //
+    base_origin: Point,
+    base_view: f64,
 }
 
 impl Buffer {
-    fn new(width: usize, height: usize) -> Self {
+    fn new(width: usize, height: usize, origin: Point, view: f64) -> Self {
         Self {
             base: vec![0; width * height].into_boxed_slice(),
             zoomed: vec![0; width * height].into_boxed_slice(),
-            multiplier: 1.0,
+            base_origin: origin,
+            base_view: view,
         }
     }
 }
 
 fn main() {
-    let w = 1000;
-    let h = 600;
+    let width = 1000;
+    let height = 600;
 
-    let mut buffer = Buffer::new(w, h);
-
-    let mut window = Window::new("Mandelbrot", w, h, WindowOptions::default()).unwrap();
+    let mut window = Window::new("Mandelbrot", width, height, WindowOptions::default()).unwrap();
 
     window.set_target_fps(60);
 
-    let mut origin_x = -1.0;
-    let mut origin_y = -1.0;
-    // displayed range / pixel size (zooming in means reducing view)
-    let mut view = 1.0 / 500.0;
+    let mut origin = Point::new(-2.5, -1.0);
+
+    // Displayed range / pixel size (zooming in means reducing view)
+    // (real world units / pixel)
+    let mut view = 1.0 / 300.0;
+
+    let mut buffer = Buffer::new(width, height, origin, view);
 
     let mut cached = None;
 
     let mut first_time = true;
 
     while window.is_open() {
-        let mut mouse01 = None;
+        let mut zoomed = false;
 
         if let Some((mouse_x, mouse_y)) = window.get_mouse_pos(MouseMode::Discard) {
             if let Some((_, scroll_y)) = window.get_scroll_wheel() {
+                zoomed = true;
+
+                // let cursor_rel = Point::new(mouse_x as f64, mouse_y as f64);
+                // let cursor_abs = origin + (cursor_rel * view).to_vector();
+
+                // let pos_x = mouse_x as f64 * view + origin.x;
+                // let pos_y = mouse_y as f64 * view + origin.y;
+
+                // origin.x = pos.x + (origin.x - pos.x) / multiplier;
+                // origin.y = pos.y + (origin.y - pos.y) / multiplier;
+
+                // origin = cursor_abs + (origin - cursor_abs) / multiplier;
+
+
+                // [0, 1] in the reference frame given by the window
+                let cursor_rel = Point::new(
+                    mouse_x as f64 / width as f64,
+                    mouse_y as f64 / height as f64,
+                );
+
+                let cursor_abs = origin + (cursor_rel * view).to_vector();
+
                 let multiplier = 1.0 + (scroll_y as f64 / 100.0).clamp(-0.2, 0.2);
-
-                mouse01 = Some((
-                    mouse_x as f64 / w as f64,
-                    mouse_y as f64 / h as f64,
-                    multiplier,
-                ));
-
                 view /= multiplier;
 
-                let pos_x = mouse_x as f64 * view + origin_x;
-                let pos_y = mouse_y as f64 * view + origin_y;
+                origin = cursor_abs + (origin - cursor_abs) / multiplier;
 
-                origin_x = pos_x + (origin_x - pos_x) / multiplier;
-                origin_y = pos_y + (origin_y - pos_y) / multiplier;
+                println!("{:?} {}", origin, view);
             }
         }
 
-        let cache_key = (origin_x, origin_y, view);
+        let cache_key = (origin, view);
         if window.is_key_pressed(Key::Space, KeyRepeat::No) {
-            render(&mut buffer.base, w, h, (origin_x, origin_y), view);
+            render(&mut buffer.base, width, height, origin, view);
             buffer.zoomed.copy_from_slice(&buffer.base);
-            buffer.multiplier = 1.0;
+            buffer.base_origin = origin;
+            buffer.base_view = view;
 
             cached = Some(cache_key);
             // window.update_with_buffer(&buffer, w, h).unwrap();
         } else {
             if cached != Some(cache_key) {
                 if first_time {
-                    render(&mut buffer.base, w, h, (origin_x, origin_y), view);
+                    render(&mut buffer.base, width, height, origin, view);
                     buffer.zoomed.copy_from_slice(&buffer.base);
-                    buffer.multiplier = 1.0;
+                    buffer.base_origin = origin;
+                    buffer.base_view = view;
 
                     first_time = false;
-                } else if let Some((center_x, center_y, multiplier)) = mouse01 {
-                    buffer.multiplier *= multiplier;
-                    sample_zoomed(&buffer.base, &mut buffer.zoomed, w, h, center_x, center_y, buffer.multiplier);
+                } else if zoomed {
+                    sample_zoomed(
+                        &buffer.base,
+                        &mut buffer.zoomed,
+                        //
+                        width,
+                        height,
+                        //
+                        buffer.base_origin,
+                        buffer.base_view,
+                        //
+                        origin,
+                        view,
+                    );
                 }
                 cached = Some(cache_key);
             }
         }
 
-        window.update_with_buffer(&buffer.zoomed, w, h).unwrap();
+        window
+            .update_with_buffer(&buffer.zoomed, width, height)
+            .unwrap();
     }
 }
